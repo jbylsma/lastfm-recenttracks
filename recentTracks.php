340ee182314433164fdddfa3ed0e6dde39af280b
Jonny Bylsma <jmbylsma@gmail.com>
2012-12-31 23:35:29 -0500

    Error handling, formatting

--- recentTracks.php
+++ recentTracks.php
@@ -1,62 +1,72 @@
-#!/usr/bin/php
 <?php
-  function fetchRecentTracks($userName) {
-    $url = "http://ws.audioscrobbler.com/1.0/user/" . $userName . "/recenttracks.rss";
-    $ch = curl_init($url);
-    $options = array(
-      CURLOPT_RETURNTRANSFER  => true,
-      CURLOPT_HEADER          => false
-    );
-    curl_setopt_array($ch, $options);
 
-    $rss = curl_exec($ch);
-    curl_close($ch);
+function fetchRecentTracks($userName) {
+  $url = "http://ws.audioscrobbler.com/1.0/user/" . $userName . "/recenttracks.rss";
+  $ch = curl_init($url);
+  $options = array(
+    CURLOPT_RETURNTRANSFER  => true,
+    CURLOPT_HEADER          => false
+  );
+  curl_setopt_array($ch, $options);
 
-    return $rss;
+  $rss = curl_exec($ch);
+  curl_close($ch);
+
+  return $rss;
+}
+
+function parseRecentTracksXml($rss) {
+  $recentTracksArray = array();
+  libxml_clear_errors();
+
+  $xml = simplexml_load_string($rss);
+  $xmlErrors = libxml_get_errors();
+  if (count($xmlErrors) > 0) {
+    return false;
   }
 
-  function parseRecentTracksXml($rss) {
-    $recentTracksArray = array();
-    $xml = simplexml_load_string($rss);
-    $data = $xml->channel;
-    foreach($data->item as $song) {
-      $title = (string) $song->title;
-      $date = (string) $song->pubDate;
-      $str = "[" . formatDate($date) . "] " . $title . "\n";
-      //$str = $title . " [" . formatDate($date) . "]" . "\n";
-      $recentTracksArray[] = $str;
-    }
+  $data = $xml->channel;
 
-    return $recentTracksArray;
+  foreach($data->item as $song) {
+    $title = (string) $song->title;
+    $date = (string) $song->pubDate;
+    $str = sprintf("[%s] %s\n", formatDate($date), $title);
+    $recentTracksArray[] = $str;
   }
 
-  function formatDate($date) {
-    $formattedDate = "";
+  return $recentTracksArray;
+}
 
-    $phpDate = strtotime($date);
-    $formattedDate = strftime("%Y-%m-%d %H:%M", $phpDate);
+function formatDate($date) {
+  $formattedDate = "";
+  $phpDate = strtotime($date);
+  $formattedDate = strftime("%Y-%m-%d %H:%M", $phpDate);
+  return $formattedDate;
+}
 
-    return $formattedDate;
-  }
+// Main
+// Require at least one username
+if ($argc < 2) {
+  print 'At least one username is required.' . "\n";
+  exit(1);
+}
 
-  function getUserName($argv) {
-    if (sizeOf($argv) == 0) {
-      $userName = "RnRG";
-    }
-    else {
-      $userName = $argv[1];
-    }
+array_shift($argv);
+$userNames = $argv;
 
-    return $userName;
-  }
+libxml_use_internal_errors(true);
 
-  // Main
-  $userName = getUserName($argv);
+foreach ($userNames as $userName) {
   $rss = fetchRecentTracks($userName);
   $recentTracksArray = parseRecentTracksXml($rss);
+
+  if (!$recentTracksArray) {
+    echo("Error with $userName\n");
+    continue;
+  }
 
   echo($userName . "::recent\n");
   foreach ($recentTracksArray as $track) {
     echo($track);
   }
-?>
+}
